cd34d4f0fc48535e73e095b85b43ea694102d2d7
Nick Greenquist <nickgreenquist.gmail.com>
2017-04-21 10:41:59 -0400

    modified shell script for unzipped folders to save results to folders marked with Nick

--- Project 2/Experiments/runAll_notZipped.sh
+++ Project 2/Experiments/runAll_notZipped.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Source: http://stackoverflow.com/a/29248777/2445901
-mkdir ResultFiles
-echo "Started learning: $(date)" > progress
+mkdir ResultFiles_Nick
+echo "Started learning: $(date)" > progress_Nick
 for D in *; do
     if [ -d "${D}" ]; then
         echo "${D}"
         cd "$D"/scripts/
         java -jar RunKeel.jar
         cd ../../
-        cp -r "$D"/results/Vis-Clas-Check/* ResultFiles
-        echo "Finished with $D: $(date)" >> progress
+        cp -r "$D"/results/Vis-Clas-Check/* ResultFiles_Nick
+        echo "Finished with $D: $(date)" >> progress_Nick
     fi
 done
-echo "Finished learning: $(date)" >> progress
+echo "Finished learning: $(date)" >> progress_Nick
 echo -e "\n"
 cat progress
