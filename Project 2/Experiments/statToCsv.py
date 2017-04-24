e304d6a1cec183e6e96f0ed53ba8386381da0445
aphedges <aphedges1@gmail.com>
2017-04-24 19:23:43 -0400

    Wrote script to convert results to CSV and ran for both sets of results

---
+++ Project 2/Experiments/statToCsv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# Author: Alex Hedges
+# This is written for Python 3.6.1.
+# Usage: statToCsv.py resultDirectory outputFile
+# resultDirectory: directory of files in stat format
+# outputFile: file to store CSV file format
+# This will not work properly if a different input format is used.
+
+import linecache
+import os
+import re
+import sys
+
+USAGE_MESSAGE = "Usage: statToCsv.py resultDirectory outputFile"
+INPUT_REGEX = r"^Classifier= (\S*)$"
+
+TESTS=["TSTAdaBoost.NC-C", "TSTC45-C", "TSTChi-RW-C", "TSTFURIA-C", "TSTGAssist-Intervalar-C", "TSTGFS-GCCL-C", "TSTRipper-C"]
+DATASETS=["abalone", "australian", "balance", "bupa", "car", "chess", "coil2000", "contraceptive", "ecoli", "flare", "german", "glass", "haberman", "heart", "ionosphere", "iris", "led7digit", "lymphography", "monk-2", "newthyroid", "pima", "ring", "saheart", "splice", "thyroid", "tic-tac-toe", "titanic", "vehicle", "vowel", "wine"]
+
+def processFile(fileName: str) -> (str, float):
+	datasetLine = linecache.getline(fileName, 3).strip()
+	scoreLine = linecache.getline(fileName, 19).strip()
+	dataset = re.search(INPUT_REGEX, datasetLine, re.M|re.I).group(1)
+	score = float(scoreLine) * 100
+	return (dataset, score)
+
+def main():
+	if len(sys.argv) != 3: # Test for arguments
+		print(USAGE_MESSAGE)
+		sys.exit()
+	resultDirectory = os.path.join(os.getcwd(), sys.argv[1])
+	if not os.path.isdir(resultDirectory):
+		raise RuntimeError("The given directory does not exist!")
+	outputFile = os.path.join(os.getcwd(), sys.argv[2])
+	results = {} # Initialize dictionary
+	for dataset in DATASETS:
+		results[dataset] = {}
+	for test in TESTS: # Read results into dictionary
+		testDir = os.path.join(resultDirectory, test)
+		for i in range(0, len(os.listdir(testDir))):
+			dataset, score = processFile(os.path.join(testDir, "result{}s0.stat".format(i)))
+			results[dataset][test] = score
+	with open(outputFile, "w", encoding="utf8") as output:
+		output.write("Dataset") # Write header row
+		for test in TESTS:
+			output.write(" ; {}".format(test))
+		output.write("\n")
+		for dataset in DATASETS: # Write individual rows
+			output.write(dataset)
+			for test in TESTS:
+				output.write(" ; {}".format(results[dataset][test]))
+			output.write("\n")
+
+if __name__ == "__main__":
+    main()
