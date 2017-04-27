f0ceb8d2406241aff15a474070d64823de007ce3
aphedges <aphedges1@gmail.com>
2017-04-27 18:03:32 -0400

    Added statistical analysis; created Eclipse project for Part 2

---
+++ Project 2/Part2/src/Classifier.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nick Greenquist
+ * @author Alex Hedges
+ *
+ */
+public class Classifier
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
